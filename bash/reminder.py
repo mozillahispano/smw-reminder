8a393e47a5ffe0befbdbad00026ff048b5867e79
cesar carruitero <cesar@mozilla.pe>
2012-05-10 00:49:34 -0500

    some changes, but with bugs

--- bash/reminder.py
+++ bash/reminder.py
@@ -6,11 +6,35 @@ json_tareas = open('tareas.json').read()
 tareas = json.loads(json_tareas)
 json_colab = open('colaboradores.json').read()
 colab = json.loads(json_colab)
-n = len(tareas["items"])
+n = len(colab["items"])
 
-# append mails with tareas respons
+# jsons structures
+# tareas['items'][0]['respon.']
+# colab['items'][0]['label']
+# colab['items'][0]['correo']
+
+# transform list in dictionary
+
+colab_new = {}
+def mailstructure(self):
+    x = [i for i in range(n)]
+    ncolab = "colab['items']["+str(varl)+"]['label']"
+    mcolab = "colab['items']["+str(varl)+"]['correo']"
+    colab_new["name"] = ncolab
+    colab_new["mail"] = mcolab
+    b = json.dumps(colab_new)
+    self.response.out.write(b)
+
+# repeat this for all
+
+#mailstructure(self,varl=0)
+
+# In colab transform <xxx arroba xxx punto xx> in <xxx@xxx.xxx>
+
+# append mails with tareas respons (new file)
 
 # send mails
+'''
 import smtplib
 import string
  
@@ -29,3 +53,4 @@ BODY = string.join((
 server = smtplib.SMTP(HOST)
 server.sendmail(FROM, [TO], BODY)
 server.quit()
+'''
