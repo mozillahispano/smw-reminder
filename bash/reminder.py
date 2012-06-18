19cd9f58161a4932ff1acb7c7d6f647b09f8e0cb
Cesar Carruitero <cesar@mozilla.pe>
2012-06-17 20:31:40 -0500

    change urllib2 instead of wget fix #4

--- bash/reminder.py
+++ bash/reminder.py
@@ -1,10 +1,11 @@
 #!/env/bin/python
 
 # load jsons
+import urllib2
 import json
-json_tareas = open('tareas.json').read()
+json_tareas = urllib2.urlopen('https://www.mozilla-hispano.org/documentacion/Especial:Ask/-5B-5BCategor%C3%ADa:Tarea-5D-5D-5B-5Bestado::!Finalizado-5D-5D/-3FResponsable%3DRespon./-3FArea/-3FProyecto/-3FEstado/-3FFechafin%3DL%C3%ADmite/mainlabel%3D/order%3DASC,ASC/sort%3DFechafin,Estado/format%3Djson').read()
 tareas = json.loads(json_tareas)
-json_colab = open('colaboradores.json').read()
+json_colab = urllib2.urlopen('https://www.mozilla-hispano.org/documentacion/Especial:Ask/-5B-5BCategoría:Colaborador-5D-5D/-3FCorreo/mainlabel%3D/format%3Djson').read()
 colab = json.loads(json_colab)
 n = len(colab["items"])
 
