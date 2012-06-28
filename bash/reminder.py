fd91e52323591972518e49c910fe6ae7aa3b869a
Cesar Carruitero <cesar@mozilla.pe>
2012-06-27 23:40:10 -0500

    order tasks for each user fix #13

--- bash/reminder.py
+++ bash/reminder.py
@@ -3,6 +3,8 @@
 
 import urllib2
 import json
+from collections import defaultdict
+
 json_tasks = urllib2.urlopen('https://www.mozilla-hispano.org/documentacion/Especial:Ask/-5B-5BCategor%C3%ADa:Tarea-5D-5D-5B-5Bestado::!Finalizado-5D-5D/-3FResponsable%3DRespon./-3FArea/-3FProyecto/-3FEstado/-3FFechafin%3DL%C3%ADmite/mainlabel%3D/order%3DASC,ASC/sort%3DFechafin,Estado/format%3Djson/limit%3D1000').read()
 tasks = json.loads(json_tasks)
 json_colab = urllib2.urlopen('https://www.mozilla-hispano.org/documentacion/Especial:Ask/-5B-5BCategoría:Colaborador-5D-5D/-3FCorreo/mainlabel%3D/format%3Djson/limit%3D1000').read()
@@ -54,6 +56,10 @@ for i in range(n):
             tasks_new.append([resp,mailresp,label])
     except KeyError:
         pass
+
+d = defaultdict(list)
+for k,v,p in tasks_new:
+    d[k,v].append(p)
 
 # send mails
 import smtplib
