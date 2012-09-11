f67f2be0414ca6a9c06b7a34ebd47983c5f43401
Cesar Carruitero <cesar@mozilla.pe>
2012-09-11 01:21:31 -0500

    add help description

--- bash/reminder.py
+++ bash/reminder.py
@@ -282,9 +282,9 @@ def meeting_today(parsed_args):
     meetingstoday()
 
 parser=argparse.ArgumentParser()
-parser.add_argument('--tasks', dest='action', action='store_const', const=tasks)
-parser.add_argument('--meetings-threedays', dest='action', action='store_const', const=meeting_threedays)
-parser.add_argument('--meetings-today', dest='action', action='store_const', const=meeting_today)
+parser.add_argument('--tasks', dest='action', action='store_const', const=tasks, help='reminder for tasks')
+parser.add_argument('--meetings-threedays', dest='action', action='store_const', const=meeting_threedays, help='reminder for meetings into three days')
+parser.add_argument('--meetings-today', dest='action', action='store_const', const=meeting_today, help= 'reminder for meetings today')
 parsed_args = parser.parse_args()
 if parsed_args.action is None:
     parser.parse_args(['-h'])
